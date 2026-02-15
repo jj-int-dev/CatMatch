5f0cacdb736932d106fdcfd995acc2f8bb0d7b1a
jj-int-dev <jjollpsl@gmail.com>
2026-02-15 09:23:25 -0500

    update error messages for user profile form

--- src/features/user-profile/validators/userProfileFormValidator.ts
+++ src/features/user-profile/validators/userProfileFormValidator.ts
@@ -4,27 +4,25 @@ import i18next from '../../../utils/i18n';
 export const createUserProfileFormValidator = () =>
   z.object({
     displayName: z
-      .string()
+      .string(i18next.t('invalid_display_name'))
       .min(1, { message: i18next.t('display_name_required') }),
-    dateOfBirth: z.union([
-      z.literal(''),
-      z.string().refine(
-        (date: string) => {
+    dateOfBirth: z.union(
+      [
+        z.literal(''),
+        z.string().refine((date: string) => {
           const parsedDate = new Date(date);
           return !isNaN(parsedDate.getTime()) && parsedDate < new Date();
-        },
-        {
-          message: i18next.t('invalid_date')
-        }
-      )
-    ]),
+        })
+      ],
+      { message: i18next.t('invalid_date') }
+    ),
     phoneNumber: z.union([z.e164(), z.literal('')], {
       message: i18next.t('invalid_phone_number')
     }),
     gender: z.enum(['Man', 'Woman', ''], {
       message: i18next.t('invalid_gender')
     }),
-    bio: z.string()
+    bio: z.string(i18next.t('invalid_bio'))
   });
 
 export type UserProfileFormSchema = z.infer<
